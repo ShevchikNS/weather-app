b743d11a9d4d2dbed6a93e9f0d9b41bc2148e926
ABC Man <Shevchikkola@gmail.com>
2022-11-25 13:21:00 +0300

    create weather app

---
+++ pages/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
+
+const Header = () => {
+    var date = new Date();
+
+    var options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+    };
+
+    return (
+        <div className="header">
+            <h2 className="MainTitle">Данные метеостанции ОАО "Гродно Азот"</h2>
+            <h1 className="SubTitle">{date.toLocaleString("Ru", options)}</h1>
+        </div>
+    );
+};
+
+export default Header;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const puppeteer = require('puppeteer')
+
+export default async function handler(req, res) {
+  const browser = await puppeteer.launch({})
+  const page = await browser.newPage()
+  await page.goto('http://old.azot.by/meteo/')
+  const result = await page.evaluate(() => {
+    const rows = document.querySelectorAll('#dewrap tr');
+    return Array.from(rows, row => {
+      const columns = row.querySelectorAll('td');
+      return Array.from(columns, column => column.innerText);
+    });
+  });
+
+  const degrees = (result[1].toString()).split(' ')[0];
+  const pressure = (result[2].toString().split(' ')[0])
+  const windSpeed = (result[3].toString().split(' ')[0])
+  const windDirection = (result[4].toString().split('\n')[0])
+  browser.close()
+  res.status(200).json({ degrees: degrees, pressure: pressure, windSpeed: windSpeed, windDirection: windDirection })
+  res.end()
 }
