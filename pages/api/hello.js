c9bc4849c304ee8f58d139bab60ab3a42e4815c7
ABC Man <Shevchikkola@gmail.com>
2022-12-13 11:55:31 +0300

    change files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,23 +1,24 @@
-
-const puppeteer = require('puppeteer')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({})
-  const page = await browser.newPage()
-  await page.goto('http://old.azot.by/meteo/')
-  const result = await page.evaluate(() => {
-    const rows = document.querySelectorAll('#dewrap tr');
-    return Array.from(rows, row => {
-      const columns = row.querySelectorAll('td');
-      return Array.from(columns, column => column.innerText);
-    });
-  });
+    const response = await axios.get('http://old.azot.by/meteo/')
+    const html = response.data
+    const $ = cheerio.load(html)
+    const values = []
 
-  const degrees = (result[1].toString()).split(' ')[0];
-  const pressure = (result[2].toString().split(' ')[0])
-  const windSpeed = (result[3].toString().split(' ')[0])
-  const windDirection = (result[4].toString().split('\n')[0])
-  browser.close()
-  res.status(200).json({ degrees: degrees, pressure: pressure, windSpeed: windSpeed, windDirection: windDirection })
-  res.end()
+
+    // values.push($('b').html())
+    $('tbody').each((index, el) => {
+        const game = $(el)
+        const title = game.find('td').text()
+        values.push(title)
+    })
+    const degrees = values[0].split('-')[1].split(' ')[0]
+    console.log(degrees)
+    res.status(200).json({degrees: degrees})
+    res.end()
 }
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,31 @@ import Footer from './Footer'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Head from 'next/head';
-// import {styles} from "./styles";
 
 export async function getServerSideProps() {
-    const resDegrees= await fetch(`http://localhost:3000/api/hello`)
-    const resSunriseSunset = await fetch(`http://localhost:3000/api/SunriseSunset`)
-
+    const resDegrees= await fetch(`${process.env.PORT}/api/hello`)
+//     // const resSunriseSunset = await fetch(`http://localhost:3000/api/SunriseSunset`)
+//
     const azot = await resDegrees.json()
-    const sunriseSunset = await resSunriseSunset.json()
-    return { props: { azot, sunriseSunset } }
+//     const sunriseSunset = await resSunriseSunset.json()
+    return { props: { azot } }
 }
-export const getSunTime = (sunriseSunset) => {
-    const date = new Date()
-    const currentHours = date.getHours()
-    const currentMinutes = date.getMinutes()
-    const parsedSunrise = (sunriseSunset.sunrise).split(':')
-    const parsedSunset = (sunriseSunset.sunset).split(':')
-    const dayLengh = (parsedSunset[0] - parsedSunrise[0]) + 0.01 * (parsedSunset[1] - parsedSunrise[1])
-    const dayCoefficient = ((currentHours - parsedSunrise[0])  + 0.01 * (currentMinutes - parsedSunrise[1]))
-    const sunTime = -40 + dayCoefficient * (100 / dayLengh)
-
-    return sunTime
-}
-export default function Home({azot, sunriseSunset}) {
-    const Style = {
-        transform: `rotate(${getSunTime(sunriseSunset)}deg)`,
-    }
+// export const getSunTime = (sunriseSunset) => {
+//     const date = new Date()
+//     const currentHours = date.getHours()
+//     const currentMinutes = date.getMinutes()
+//     const parsedSunrise = (sunriseSunset.sunrise).split(':')
+//     const parsedSunset = (sunriseSunset.sunset).split(':')
+//     const dayLengh = (parsedSunset[0] - parsedSunrise[0]) + 0.01 * (parsedSunset[1] - parsedSunrise[1])
+//     const dayCoefficient = ((currentHours - parsedSunrise[0])  + 0.01 * (currentMinutes - parsedSunrise[1]))
+//     const sunTime = -40 + dayCoefficient * (100 / dayLengh)
+//
+//     return sunTime
+// }
+export default function Home({azot}) {
+//     const Style = {
+//         transform: `rotate(${getSunTime(sunriseSunset)}deg)`,
+//     }
 
     return (
 
@@ -39,12 +38,12 @@ export default function Home({azot, sunriseSunset}) {
                 <div className="now">
                         <div className="now-astro">
                             <div className="now-astro-sun">
-                                <div className="now-astro-line now-astro-line-day" style={Style}></div>
+                                <div className="now-astro-line now-astro-line-day"></div>
                             </div>
                             <div className="sunriseSunset">
                                 <div className="now-astro-sunrise">
                                     <div className="time">
-                                        {sunriseSunset.sunrise}
+                                        {/*{sunriseSunset.sunrise}*/}
                                     </div>
                                     <div className="caption">
                                         Восход
@@ -52,7 +51,7 @@ export default function Home({azot, sunriseSunset}) {
                                 </div>
                                 <div className="now-astro-sunset">
                                     <div className="time">
-                                        {sunriseSunset.sunset}
+                                        {/*{sunriseSunset.sunset}*/}
                                     </div>
                                     <div className="caption">
                                         Заход
@@ -62,16 +61,12 @@ export default function Home({azot, sunriseSunset}) {
                         </div>
                 </div>
                 <div className="mainData">
-                    <h1 className="degrees">{azot.degrees}</h1>
-                    <h1 className = "weatherNow">{sunriseSunset.weatherNow}</h1>
+                    <h1 className="degrees"> {azot.degrees}</h1>
+                    <h1 className = "weatherNow"></h1>
                 </div>
                 {/*<h2>Осадки: {currentPrecipitation[date.getHours()]}</h2>*/}
             </div>
-            <Footer
-                pressure = {azot.pressure}
-                windSpeed = {azot.windSpeed}
-                windDirection = {azot.windDirection}
-            />
+
         </div>
 
     )
