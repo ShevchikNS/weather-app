26ddddda5dd55a18f35d14cc83410bda2829d058
ABC Man <Shevchikkola@gmail.com>
2022-12-30 14:55:58 +0300

    move files

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,33 @@ import Header from './Header'
 import Image from 'next/image'
 import Head from 'next/head';
 import Footer from './Footer'
-import {checkWeatherWithCode, getWindSpeed, getSunTime} from './services/checkWeather.js'
+import {checkWeatherWithCode, getWindSpeed, getSunTime} from '../services/checkWeather.js'
 
 export async function getServerSideProps() {
     const resDegrees = await fetch(`${process.env.PORT}/api/weatherApi`)
-    const weatherFromApi = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=53.63&longitude=23.89&daily=sunrise,sunset&current_weather=true&windspeed_unit=ms&timezone=Europe%2FMoscow`)
+    const resWeatherFromApi = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=53.63&longitude=23.89&daily=sunrise,sunset&current_weather=true&windspeed_unit=ms&timezone=Europe%2FMoscow`)
 
-    const weather = await resDegrees.json()
-    const weather2 = await weatherFromApi.json()
+    const weatherFromScraper = await resDegrees.json()
+    const weatherFromApi = await resWeatherFromApi.json()
 
-    const weatherCode = weather2.current_weather.weathercode
+    const weatherCode = weatherFromApi.current_weather.weathercode
 
-    return {props: {weather, weather2}}
+    return {props: {weatherFromScraper, weatherFromApi}}
 }
 
-export default function Home({weather, weather2}) {
+export default function Home({weatherFromScraper, weatherFromApi}) {
     const [currentWeather, setCurrentWeather] = useState('')
     const Style = {
-        transform: `rotate(${getSunTime(weather2)}deg)`,
+        transform: `rotate(${getSunTime(weatherFromApi)}deg)`,
     }
 
-    const sunrise = (weather2.daily.sunrise[0])
+    const sunrise = (weatherFromApi.daily.sunrise[0])
     const sunriseWithoutData = sunrise.slice(sunrise.length - 5)
     const parsedSunrise = sunriseWithoutData.split(':')
     const convertedSunrise = Number(parsedSunrise)
 
     useEffect(() => {
-        const resultWeatherCheck = checkWeatherWithCode(weather2.current_weather.weathercode)
+        const resultWeatherCheck = checkWeatherWithCode(weatherFromApi.current_weather.weathercode)
         setCurrentWeather(resultWeatherCheck)
     });
     return (
@@ -50,7 +50,7 @@ export default function Home({weather, weather2}) {
                         <div className="sunriseSunset">
                             <div className="now-astro-sunrise">
                                 <div className="time">
-                                    {(weather2.daily.sunrise[0]).slice((weather2.daily.sunrise[0]).length - 5)}
+                                    {(weatherFromApi.daily.sunrise[0]).slice((weatherFromApi.daily.sunrise[0]).length - 5)}
                                 </div>
                                 <div className="caption">
                                     {convertedSunrise > 10 ? <p>Заход</p> : <p>Восход</p>}
@@ -58,7 +58,7 @@ export default function Home({weather, weather2}) {
                             </div>
                             <div className="now-astro-sunset">
                                 <div className="time">
-                                    {(weather2.daily.sunset[0]).slice((weather2.daily.sunset[0]).length - 5)}
+                                    {(weatherFromApi.daily.sunset[0]).slice((weatherFromApi.daily.sunset[0]).length - 5)}
                                 </div>
                                 <div className="caption">
                                     {convertedSunrise > 10 ? <p>Восход</p> : <p>Заход</p>}
@@ -68,14 +68,14 @@ export default function Home({weather, weather2}) {
                     </div>
                 </div>
                 <div className="mainData">
-                    <div className="degrees"> {weather.degrees}°C</div>
+                    <div className="degrees"> {weatherFromScraper.degrees}°C</div>
                     <div className="weatherNow"> {currentWeather}</div>
                 </div>
             </div>
             <Footer
-                pressure={weather.pressure}
-                windSpeed={weather.windSpeed}
-                windDirection={getWindSpeed(weather.windDirection)}
+                pressure={weatherFromScraper.pressure}
+                windSpeed={weatherFromScraper.windSpeed}
+                windDirection={getWindSpeed(weatherFromScraper.windDirection)}
             />
         </div>
     )
